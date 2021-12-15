edaa08d317e74ce4d6f8508a721ed533c4e51e01
0xLil14n <0xLil14n@gmail.com>
2021-12-15 12:21:12 -0500

    adding metamask login

---
+++ components/MetamaskLogin/MetamaskLogin.tsx
@@ -0,0 +1,48 @@
+import { useMoralis } from 'react-moralis';
+import { useEffect } from 'react';
+const MetamaskLogin = () => {
+  const {
+    isWeb3Enabled,
+    isAuthenticated,
+    enableWeb3,
+    authenticate,
+    logout,
+    isAuthenticating,
+    isWeb3EnableLoading,
+  } = useMoralis();
+  const isLoading = isAuthenticating || isWeb3EnableLoading;
+  const isLoggedIn = isAuthenticated;
+  const enableAndAuthenticate = async () => {
+    await enableWeb3();
+    await authenticate();
+  };
+  const signInOrSignOut = () => {
+    if (!isWeb3Enabled || !isAuthenticated) {
+      enableAndAuthenticate();
+    } else {
+      logout();
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      enableWeb3();
+    }
+  }, [isAuthenticated]);
+
+
+
+  return (
+    <Button
+      disabled={isAuthenticating}
+      onClick={signInOrSignOut}
+    >
+      {isLoading
+        ? 'Loading...'
+        : isLoggedIn
+        ? 'Disconnect'
+        : 'Connect and Log In'}
+    </Button>
+  );
+};
+export default MetamaskLogin;
